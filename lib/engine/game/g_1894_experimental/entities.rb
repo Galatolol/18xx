4611117cd930425947436c0efdc1b8dc4afd4c7e
Galatolol <Galatolol@protonmail.com>
2022-10-28 21:41:01 +0200

    New stock market

--- lib/engine/game/g_1894_experimental/entities.rb
+++ lib/engine/game/g_1894_experimental/entities.rb
@@ -76,7 +76,7 @@ module Engine
             name: 'Ligne de Saint-Quentin à Guise',
             sym: 'SQG',
             value: 100,
-            desc: 'Revenue is equal to 90 if Saint-Quentin (G10) is green, to 120 if Saint-Quentin is brown and to 0 otherwise.'\
+            desc: 'Revenue is equal to 70 if Saint-Quentin (G10) is green, to 100 if Saint-Quentin is brown and to 0 otherwise.'\
                   ' Closes in purple phase.',
             abilities: [{ type: 'close', on_phase: 'Purple' }],
             color: '#d9d9d9',
@@ -232,14 +232,6 @@ module Engine
                 type: 'description',
                 description: 'May not redeem shares',
               },
-              {
-                type: 'description',
-                description: 'Each route +10 F per revenue center',
-              },
-              # {
-              #   type: 'description',
-              #   description: 'Revenue +10/30/50/80 if 2/4/6/8 stops',
-              # },
             ],
           },
           {
--- lib/engine/game/g_1894_experimental/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_1894_experimental/step/buy_sell_par_shares.rb
@@ -13,10 +13,6 @@ module Engine
             super
           end
 
-          def get_par_prices(_entity, corp)
-            @game.par_prices(corp)
-          end
-
           def can_buy_multiple?(entity, corporation, _owner)
             super && corporation.owner == entity && num_shares_bought(corporation) < 2
           end
